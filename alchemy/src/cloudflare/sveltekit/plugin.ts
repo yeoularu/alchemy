ffd1c7c659d8fd689accc9b4e27b963618312750
John Royal <34844819+john-royal@users.noreply.github.com>
2026-02-18 11:33:38 -0500

    disable plugin during svelte-kit sync

--- alchemy/src/cloudflare/sveltekit/plugin.ts
+++ alchemy/src/cloudflare/sveltekit/plugin.ts
@@ -1,14 +1,16 @@
 import adapter, { type AdapterOptions } from "@sveltejs/adapter-cloudflare";
 import type { Adapter } from "@sveltejs/kit";
 import { getPlatformProxyOptions } from "../cloudflare-env-proxy.ts";
-import { withSkipPathValidation } from "../miniflare/paths.ts";
 
 const isSvelteLanguageServer = !!process.argv.find((arg) =>
   arg.includes("svelte-language-server"),
 );
+const isSvelteKitSync =
+  !!process.argv.find((arg) => arg.includes("svelte-kit")) &&
+  process.argv.includes("sync");
 
 export default (options?: AdapterOptions): Adapter => {
-  if (isSvelteLanguageServer) {
+  if (isSvelteLanguageServer || isSvelteKitSync) {
     // the svelte language server runs from the root and therefore may be running in a different cwd than the `Website.cwd`
     // for now, we work around this by returning a noop adapter to avoid breaking svelte intellisense
     return {
@@ -17,15 +19,7 @@ export default (options?: AdapterOptions): Adapter => {
     };
   }
   const { platformProxy: proxyOptions, ...config } = options ?? {};
-  const hasCustomConfigPath =
-    typeof proxyOptions === "object" &&
-    proxyOptions !== null &&
-    typeof proxyOptions.configPath === "string";
-
-  const platformProxy = hasCustomConfigPath
-    ? getPlatformProxyOptions(proxyOptions)
-    : withSkipPathValidation(() => getPlatformProxyOptions(proxyOptions));
-
+  const platformProxy = getPlatformProxyOptions(proxyOptions);
   return adapter({
     platformProxy,
     config: platformProxy.configPath,
